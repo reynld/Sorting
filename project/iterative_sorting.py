7824e058fb95c867a72020336138e8db68ee420f
Chris Cedeno <chrisrey95@gmail.com>
2019-02-19 12:52:45 -0500

    bubble sorted

--- project/iterative_sorting.py
+++ project/iterative_sorting.py
@@ -44,7 +44,14 @@ print(arr);
 
 # STRETCH: implement the Bubble Sort function below
 def bubble_sort( arr ):
+    is_sorted = False
+    while not is_sorted:
+        is_sorted = True
 
+        for i in range(len(arr) - 1):
+            if arr[i] > arr[i + 1]:
+                arr[i], arr[i + 1] = arr[i + 1], arr[i]
+                is_sorted = False
     return arr
 
 
